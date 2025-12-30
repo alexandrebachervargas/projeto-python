807410766045a4af00768679febba63465bd81e9
alexandre-bacher-vargas <alexandrebachervargas@gmail.com>
2025-12-29 21:18:24 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Bem-vindo ao meu primeiro projeto em Python!")
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print(f"{nome}, você é maior de idade.")
+else:
+    print(f"{nome}, você é menor de idade.")
